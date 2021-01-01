14e6520da5d3f2c3b44247e0e83ab9d3a4d225a6
Richard Dallaway <richard@dallaway.com>
2021-01-01 10:17:56 +0000

    add experiment id

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,27 @@ struct Settings {
     seed: u64,
 }
 
+fn experiment_id(settings: &Settings, version: &str) -> String {
+    let settings_text = format!("{:?}", &settings);
+    format!("{}_{}", version, hash(settings_text))
+}
+
+fn hash(s: String) -> u64 {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::Hasher;
+
+    let mut h = DefaultHasher::new();
+    h.write(s.as_bytes());
+    h.finish()
+}
+
 
 fn main() {
     let settings = Settings::from_args();
     println!("{:#?}", settings);
     println!("{}", GIT_VERSION);
+
+    let id = experiment_id(&settings, GIT_VERSION);
+    println!("{}", id);
+
 }
